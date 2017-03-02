39e631ce68808da803212adb8e80a2527eee73e7
zicjin <zicjin@gmail.com>
2017-03-02 20:10:20 +0800

    .

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,64 @@
 // TODO add startup code here
+
 app.selectedCities = localStorage.selectedCities;
 if (app.selectedCities) {
-  app.selectedCities = JSON.parse(app.selectedCities);
-  app.selectedCities.forEach(function(city) {
-    app.getForecast(city.key, city.label);
-  });
-} else {
-  /* The user is using the app for the first time, or the user has not
-    * saved any cities, so show the user some fake data. A real app in this
-    * scenario could guess the user's location via IP lookup and then inject
-    * that data into the page.
-    */
-  app.updateForecastCard(initialWeatherForecast);
   app.selectedCities = [
-    {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    {key: "2132574", label: "杭州"}
   ];
   app.saveSelectedCities();
 }
 
+app.selectedCities = JSON.parse(app.selectedCities);
+app.selectedCities.forEach(function(city) {
+  app.getForecast(city.key, city.label);
+});
+
 // TODO add service worker code here
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-            .register('./service-worker.js')
-            .then(function() { console.log('Service Worker Registered'); });
+    .register('./service-worker.js')
+    .then(function() { console.log('Service Worker Registered'); });
 }
+
+/*
+  * Fake weather data that is presented when the user first uses the app,
+  * or when the user has not saved any cities. See startup code for more
+  * discussion.
+  */
+// var initialWeatherForecast = {
+//   key: '2459115',
+//   label: 'New York, NY',
+//   created: '2016-07-22T01:00:00Z',
+//   channel: {
+//     astronomy: {
+//       sunrise: "5:43 am",
+//       sunset: "8:21 pm"
+//     },
+//     item: {
+//       condition: {
+//         text: "Windy",
+//         date: "Thu, 21 Jul 2016 09:00 PM EDT",
+//         temp: 56,
+//         code: 24
+//       },
+//       forecast: [
+//         {code: 44, high: 86, low: 70},
+//         {code: 44, high: 94, low: 73},
+//         {code: 4, high: 95, low: 78},
+//         {code: 24, high: 75, low: 89},
+//         {code: 24, high: 89, low: 77},
+//         {code: 44, high: 92, low: 79},
+//         {code: 44, high: 89, low: 77}
+//       ]
+//     },
+//     atmosphere: {
+//       humidity: 56
+//     },
+//     wind: {
+//       speed: 25,
+//       direction: 195
+//     }
+//   }
+// };
+// TODO uncomment line below to test app with fake data
+// app.updateForecastCard(initialWeatherForecast);
